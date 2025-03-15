d200950a9809e54221cc64575ebb8859e150a182
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-03-15 17:42:17 +1300

    delete sanicle-ai folder

--- sanicle-ai-presentation/sanicle-ai/app/hr-dashboard/[userId]/events/edit/[eventId]/components/LoadingView.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function LoadingView() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-lg">Loading event information...</div>
-    </div>
-  );
-} 
--- sanicle-ai-presentation/sanicle-ai/components/custom/PeriodCalendar.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-// Export from restructured component directory
-export { PeriodCalendar } from "./PeriodCalendar/index";
-export type { PeriodRecord } from "./PeriodCalendar/types"; 
